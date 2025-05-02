ab288fb7f64362f269c434b089c01cafdaecfb1b
Safwan146 <Safwanalsadaf17@gmail.com>
2025-05-02 18:41:22 +0600

    Ummah😘
    Hi baby

---
+++ safwan.py
@@ -0,0 +1,15 @@
+# hello_mahid.py
+
+def greet():
+    name = "Mahid"
+    print(f"Hello, {name}! This is your first Python script from GitHub to Termux!")
+
+if __name__ == "__main__":
+    greet()# hello_mahid.py
+
+def greet():
+    name = "Mahid"
+    print(f"Hello, {name}! This is your first Python script from GitHub to Termux!")
+
+if __name__ == "__main__":
+    greet()
